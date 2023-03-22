ff6b7078f5c567f67cb677ece3b0d7c5a63a8eed
Richard Miao <miaoshiwei2008@gmail.com>
2023-03-22 17:45:41 +0800

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("hello world.");
+})
+
+app.listen(3000, ()=>{
+    console.log("server started at port: 3000")
+});
